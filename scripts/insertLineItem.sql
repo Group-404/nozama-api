71fa851d3b3cffd5a806e136aaafba7935a99715
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-24 19:50:38 -0400

    adds product.id to test script insertLineItem.sql.

--- scripts/insertLineItem.sql
+++ scripts/insertLineItem.sql
@@ -1,7 +1,10 @@
-INSERT INTO "LineItems"(quantity, "createdAt", "updatedAt", "OrderId")
-  SELECT 1, clock_timestamp(), clock_timestamp(), o.id
+INSERT INTO "LineItems"(quantity, "createdAt", "updatedAt", "OrderId", "ProductId")
+  SELECT 1, clock_timestamp(), clock_timestamp(), o.id, (SELECT p.id
+                                                         FROM "Products" p
+                                                         WHERE p.name = 'mountain bike')
   FROM "Orders" o
   INNER JOIN "Users" u
   ON u.id = o."UserId"
   WHERE u.email = 'klk@klk.com'
   LIMIT 1;
+
