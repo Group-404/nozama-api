e39f687cb46f4ff66f5ac8e558bb27c44ebc1b5f
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-25 14:33:47 -0400

    checkout route now creates and displays lineItem

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models/index');
 
-// Should require authentication
+// Should require authentication?
 
 router.route('/')
  // Get method not allowed
@@ -11,13 +12,13 @@ router.route('/')
 
   // Create lineItems using product id, quantity from CART
   .post(function(req, res){
-    res.send("We created lineItems");
-    // models.user.js.create(req.body).then(function(user){
-    //  res.json(cat);
-   //   function(err) {
-   //   console.log(err);
-   // }
- });
+    // res.send("We created lineItems");
+    models.LineItem.create(req.body).then(function(lineItem){
+        res.json(lineItem);
+      }, function(err){
+        console.log(err);
+    });
+  });
 
 
 module.exports = router;
