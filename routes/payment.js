f8406376b8536da0292f20dc1685837ee983b728
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-25 18:06:37 -0400

    async waterfall creates order and creates line items!
    still need to add functions to waterfall to:
    1) calculate total cost, 2) create payment.

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models/index');
+var async = require('async');
 
 // Should require authentication?
 
@@ -10,20 +11,55 @@ router.route('/')
     res.sendStatus(405);
   })
 
-  // Create lineItems using array of objects {product id, quantity} from CART
-  // send req.body as JSON array
   .post(function(req, res, next){
-  //   console.log(req.body);
-  //   models.LineItem.bulkCreate(req.body).then(function(lineItems){
-  //       res.json(lineItems);
-  //     }, function(err){
-  //       next(err);
-  //   });
-    models.LineItem.create(req.body).then(function(lineItem){
-      res.json(lineItem);
-      }, function(err){
-        next(err);
-    });
+    // if user is logged in -- do I check for userid in session?
+
+    async.waterfall([
+      // creates order
+      function(done){
+        models.Order.create({}).then(function(order){
+          // facilitates handoff between this funciton and the next
+          done(null, order);
+        }, function(err){
+          done(err);
+        });
+      },
+      function(order, done){
+        // set order's user id to current user's id
+        models.User.findById(1).then(function(user){
+          order.setUser(user).then(function(order){
+            done(null, order);
+          }, function(err){
+            done(err);
+          });
+        })
+
+      },
+      //creates lineItems using JSON array of objects from
+      // cart should be exact representation of lineItems
+      function(order, done){
+        // var items = JSON.parse(req.body.cart);
+        req.body.cart.forEach(function(item){
+          item.OrderId = order.id;
+        });
+
+        models.LineItem.bulkCreate(req.body.cart).then(function(lineItems){
+          console.log(lineItems);
+          done(null, {order: order, lineItems: lineItems});
+        }, function(err){
+          done(err);
+        });
+      }
+      ],function(err, result){
+        if (err) {
+          return next(err);
+        }
+        res.json(result);
+      }); // waterfall END
+
+    // add to waterfall: calculate total
+
+    // add to waterfall: create payment
 
   });
 
