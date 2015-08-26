ae049bb520f42e8cc927dbb7983c61e6f4467271
kristenlk <klkehlenbeck@gmail.com>
2015-08-25 21:48:02 -0400

    Adds a .all and .get for profiles/:id.
    .get not working perfectly yet - haven't figured out how to send back both the user and profile information together, but tested both separately.

--- routes/profiles.js
+++ routes/profiles.js
@@ -6,54 +6,42 @@ var models = require('../models/index');
 //
 
 router.route('/')
- // Get profile / user information
   .get(function(req, res, next) {
     res.send('Here are the profiles');
   })
 
-  // res.json(req.user.profile)
 
-  // Create profile (not user information like email and password)
+router.route('/:id')
+  // "before action" - grab profile id
+  .all(function(req, res, next) {
+    models.Profile.findById(req.params.id).then(function(profile){
+      res.locals.profile = profile;
+      next();
+    }, function(err){
+      next(err);
+    })
+  })
 
- //  .post(function(req, res, next){
+// can't get user and profile to render together
+  .get(function(req, res){
+    models.Profile.findById(req.params.id).then(function(profile){
+      // res.json(req.params.id);
+      profile.getUser().then(function(user){
+        res.send(user);
+        // res.send(profile, user);
+      });
 
- //    var blankProfile = { // when a user creates an account, a blank profile will be associated with it
- //      lastName: '',
- //      firstName: '',
- //      addressOne: '',
- //      addressTwo: '',
- //      // drop tables so I don't have to include addressTwo here
- //      city: '',
- //      state: '',
- //      zipCode: '',
- //      phoneNumber: ''
- //    }
+    }, function(err){
+      console.error(err);
+    });
+  })
 
- //    models.Profile.create(blankProfile).then(function(profile){
- //      res.json(profile);
- //      // call this after a user is created - associate this blank profile with the user that was just created - getUser()?
- //    }, function(err) {
- //      next(err);
- //    });
-
- //    // res.send(req.params);
- //    // res.send("We created a profile!");
- //    // models.user.js.create(req.body).then(function(user){
- //    //  res.json(cat);
- //   //   function(err) {
- //   //   console.log(err);
- //   // }
- // });
 
 // Update profile / user information
 
+
+
 // Delete profile / user information
 
-
-
-// // POST new user
-// router.post('/', function(req, res){
-//   res.send()
-// })
 
 module.exports = router;
