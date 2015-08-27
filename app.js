0813383c3bec6d8ad6a945e2970f69f14208faa6
kristenlk <klkehlenbeck@gmail.com>
2015-08-27 11:16:57 -0400

    Updates cors policy to allow cookie and content-type headers.

--- app.js
+++ app.js
@@ -21,14 +21,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(cors());
+// app.use(cors());
 
-
+app.use(cors({
+ credentials: true,
+ origin: 'http://localhost:5000',
+ allowedHeaders: ['Cookie', 'Content-Type']
+}));
 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
